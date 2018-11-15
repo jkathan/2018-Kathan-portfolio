35f86dfa3d0f56746bbcb626f8c6e6899c2305a8
Joey Kathan <kathan.joey@gmail.com>
2018-11-15 17:00:20 -0500

    adding styling

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 export default function Contacts(){
 	return (
-                <div id="contact">
+                <div className="contact">
                     <h2 className='section-heading'>Contact</h2>
                     <hr />
                     <ul >
-                        <a href="mailto:kathan.joey@gmail.com"><li><img src="https://i.imgur.com/wM05Dap.png" alt="email logo" className='contactImages'/></li></a>
-                        <a href="https://twitter.com/joeykathan"><li><img src="https://i.imgur.com/IGngRbt.png" alt="twitter logo" className='contactImages'/></li></a>
-                        <a href="https://github.com/jkathan"><li><img src="https://i.imgur.com/fuLVv1g.png" alt="github logo" className='contactImages'/></li></a>
-                        <a href="https://www.linkedin.com/in/joseph-kathan-07452361/"><li><img src="https://i.imgur.com/X3tWkNl.png" alt="linkedin logo" className='contactImages'/></li></a>
+                        <a href="mailto:kathan.joey@gmail.com" target="_blank"><li><img src="https://i.imgur.com/wM05Dap.png" alt="email logo" className='contactImages'/></li></a>
+                        <a href="https://twitter.com/joeykathan" target="_blank"><li><img src="https://i.imgur.com/IGngRbt.png" alt="twitter logo" className='contactImages'/></li></a>
+                        <a href="https://github.com/jkathan" target="_blank"><li><img src="https://i.imgur.com/fuLVv1g.png" alt="github logo" className='contactImages'/></li></a>
+                        <a href="https://www.linkedin.com/in/joseph-kathan-07452361/" target="_blank"><li><img src="https://i.imgur.com/X3tWkNl.png" alt="linkedin logo" className='contactImages'/></li></a>
                     </ul>
                 </div>
 	);
