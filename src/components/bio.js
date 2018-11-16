426aeaff591b978864d84be81fd5b73981cfbee5
Joey Kathan <kathan.joey@gmail.com>
2018-11-16 11:08:51 -0500

    adding styling

--- src/components/bio.js
+++ src/components/bio.js
@@ -4,7 +4,7 @@ export default function Bio(){
 	return (
 		<div className='bio'>
 			<p>
-			Coming from a family of artists, I fell in love with web design due to its ability to mesh creativity with interactivity and user experience. I geek out on cool new CSS tricks and animations. I am constantly striving to learn new languages, libraries, and logic to create the ideal web experience.  
+			Coming from a family of artists, I fell in love with web design due to how it elegantly meshes creativity with interactivity and user experience. I geek out on cool new CSS tricks and animations. I am constantly striving to learn new languages, libraries, and logic to create the ideal web experience.  
             </p>
             <p>
             Outside of coding, you can find me on the ultimate frisbee field throwing a disc, working on my bike, or getting yeast to play nicely with teas and grains (kombucha and beer).  I strive to bring positivity and community into all things I do.
