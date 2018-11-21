13df85a502a27bcb9ac34a696d34419ea74399bf
Joey Kathan <kathan.joey@gmail.com>
2018-11-20 21:17:50 -0500

    adding edits

--- src/components/brief.js
+++ src/components/brief.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function TagLine(){
 	return (
 		<div className='banner' id="home">
-			<h1>Hi, I'm Joey Kathan and I'm a Web Designer.</h1>
+			<h1>Hi, I'm Joey Kathan and I'm a Web Developer.</h1>
 		</div>
 	);
 }
